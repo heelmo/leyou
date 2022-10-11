d191974db1ba5a9567f32fa3507880fd7ee3aa2f
hexinyu <1271215110@qq.com>
2022-10-11 19:16:57 +0800

    修复

---
+++ leyou/leyou-registry/src/main/java/com/xunfang/LeyouRegistryApplication.java
@@ -0,0 +1,14 @@
+package com.xunfang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer   //告诉springboot启动器这是一个eureka服务端
+public class LeyouRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouRegistryApplication.class, args);
+    }
+}
